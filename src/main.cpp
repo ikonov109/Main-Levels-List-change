b345390314544829437824da8351ad42ef14abcb
MusikMe <vladimirikonov06@xmail.ru>
2026-09-23 09:34:12 +0800

    Refactor level management and UI components

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/LevelTools.hpp>
 #include <Geode/modify/LevelPage.hpp>
-#include <Geode/modify/LevelSelectLayer.hpp>
-#include <Geode/ui/TextInput.hpp>
-#include <Geode/ui/Popup.hpp>
 #include <matjson.hpp>
 #include <fstream>
 #include <sstream>
@@ -38,6 +36,7 @@ static void loadConfig() {
     g_configPath = (Mod::get()->getSaveDir() / "config.json").string();
 
     if (!std::filesystem::exists(g_configPath)) {
+        // Пример: кастомные ID 9001, 9002
         g_custom[1] = {"My Stereo Madness", 5, 3};
         g_custom[2] = {"Back On Track X", 10, 4};
         g_custom[3] = {"Polargeist Pro", 7, 2};
@@ -68,120 +67,62 @@ static void loadConfig() {
     }
 }
 
-// ===================== UI =====================
+// ===================== LevelTools =====================
 
-class MLCWindow : public geode::Popup {
-protected:
-    TextInput* m_nameInput = nullptr;
-    TextInput* m_starsInput = nullptr;
-    TextInput* m_diffInput = nullptr;
-    TextInput* m_idInput = nullptr;
-
-    bool init(float width, float height) {
-        if (!Popup::init(width, height))
-            return false;
-
-        this->setTitle("Main Levels Config");
-
-        auto winSize = m_mainLayer->getContentSize();
-
-        // ID
-        m_idInput = TextInput::create(80.f, "ID");
-        m_idInput->setPosition({winSize.width / 2 - 120.f, winSize.height - 70.f});
-        m_idInput->setID("mlc-id");
-        m_mainLayer->addChild(m_idInput);
-
-        // Name
-        m_nameInput = TextInput::create(180.f, "Name");
-        m_nameInput->setPosition({winSize.width / 2 + 30.f, winSize.height - 70.f});
-        m_nameInput->setID("mlc-name");
-        m_mainLayer->addChild(m_nameInput);
-
-        // Stars
-        m_starsInput = TextInput::create(100.f, "Stars");
-        m_starsInput->setPosition({winSize.width / 2 - 70.f, winSize.height - 120.f});
-        m_starsInput->setID("mlc-stars");
-        m_mainLayer->addChild(m_starsInput);
-
-        // Difficulty
-        m_diffInput = TextInput::create(100.f, "Diff 0-5");
-        m_diffInput->setPosition({winSize.width / 2 + 70.f, winSize.height - 120.f});
-        m_diffInput->setID("mlc-diff");
-        m_mainLayer->addChild(m_diffInput);
-
-        // Save
-        auto saveBtn = CCMenuItemSpriteExtra::create(
-            ButtonSprite::create("Save"),
-            this,
-            menu_selector(MLCWindow::onSave)
-        );
-        auto menu = CCMenu::create();
-        menu->addChild(saveBtn);
-        menu->setPosition({winSize.width / 2, winSize.height - 175.f});
-        m_mainLayer->addChild(menu);
-
-        return true;
-    }
-
-    void onSave(CCObject*) {
-        if (!m_idInput || !m_nameInput || !m_starsInput || !m_diffInput) return;
-
-        int id = 0;
-        try { id = std::stoi(m_idInput->getString()); } catch (...) { return; }
-
-        CustomLevel lvl;
-        lvl.name = m_nameInput->getString();
-        try { lvl.stars = std::stoi(m_starsInput->getString()); } catch (...) { lvl.stars = 0; }
-        try { lvl.difficulty = std::stoi(m_diffInput->getString()); } catch (...) { lvl.difficulty = 0; }
-
-        g_custom[id] = lvl;
-        saveConfig();
-
-        log::info("Saved ID {}: {}", id, lvl.name);
-    }
-
-public:
-    static MLCWindow* create() {
-        auto ret = new MLCWindow();
-        if (ret && ret->init(420.f, 280.f)) {
-            ret->autorelease();
-            return ret;
+class $modify(MyLevelTools, LevelTools) {
+    // Расширяем список ID официальных уровней
+    static gd::unordered_set<int> getLevelList() {
+        auto list = LevelTools::getLevelList();
+        for (auto& [id, lvl] : g_custom) {
+            // Добавляем только ID, которых нет в оригинальном списке
+            if (id > 1000) {
+                list.insert(id);
+            }
         }
-        delete ret;
-        return nullptr;
+        return list;
+    }
+
+    // Для кастомных ID создаём уровень с нуля
+    static GJGameLevel* getLevel(int id, bool noString) {
+        auto it = g_custom.find(id);
+
+        if (it != g_custom.end() && id > 1000) {
+            // Создаём новый уровень
+            auto level = GJGameLevel::create();
+            if (!level) return LevelTools::getLevel(id, noString);
+
+            level->m_levelID = id;
+            level->m_levelName = it->second.name;
+            level->m_stars = it->second.stars;
+            level->m_difficulty = (GJDifficulty)it->second.difficulty;
+            level->m_levelType = GJLevelType::Main;
+            level->m_levelDesc = "Custom level";
+            level->m_creatorName = "Mod";
+            level->m_audioTrack = 0;
+            level->m_songID = 0;
+            level->m_coins = 0;
+            level->m_levelLength = 3;
+            level->m_isUploaded = false;
+            level->m_levelString = "";
+
+            log::info("Created custom level ID {}: {}", id, it->second.name);
+            return level;
+        }
+
+        auto level = LevelTools::getLevel(id, noString);
+        if (!level) return level;
+
+        // Подмена существующих уровней (1-21)
+        if (it != g_custom.end()) {
+            level->m_levelName = it->second.name;
+            level->m_stars = it->second.stars;
+            level->m_difficulty = (GJDifficulty)it->second.difficulty;
+        }
+        return level;
     }
 };
 
-// ===================== Кнопка в меню =====================
-
-class $modify(MyLevelSelectLayer, LevelSelectLayer) {
-    bool init(int page) {
-        if (!LevelSelectLayer::init(page)) return false;
-
-        auto btn = CCMenuItemSpriteExtra::create(
-            ButtonSprite::create("MLC"),
-            this,
-            menu_selector(MyLevelSelectLayer::onOpenMLC)
-        );
-        btn->setID("mlc-button");
-
-        if (auto menu = this->getChildByID("bottom-menu")) {
-            menu->addChild(btn);
-        } else {
-            auto newMenu = CCMenu::create();
-            newMenu->addChild(btn);
-            newMenu->setPosition({50, 50});
-            this->addChild(newMenu);
-        }
-        return true;
-    }
-
-    void onOpenMLC(CCObject*) {
-        MLCWindow::create()->show();
-    }
-};
-
-// ===================== Применение к уровням =====================
+// ===================== LevelPage (fallback) =====================
 
 class $modify(MyLevelPage, LevelPage) {
     void updateDynamicPage(GJGameLevel* level) {
@@ -210,5 +151,5 @@ class $modify(MyLevelPage, LevelPage) {
 
 $on_mod(Loaded) {
     loadConfig();
-    log::info("Main Levels List Change loaded");
+    log::info("Main Levels Config loaded, {} custom levels", g_custom.size());
 }
